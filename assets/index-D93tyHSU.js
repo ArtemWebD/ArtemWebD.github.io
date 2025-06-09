3c4b2cddd5ef42752ff7b41b50b60e42a87c6184
artemWebD <artemtoptun@gmail.com>
2025-06-09 12:48:08 +0700

    Обновленная версия

--- assets/index-D93tyHSU.js
+++ assets/index-D93tyHSU.js
@@ -1,17 +1,17 @@
-var _=Object.defineProperty;var u=(r,e,s)=>e in r?_(r,e,{enumerable:!0,configurable:!0,writable:!0,value:s}):r[e]=s;var a=(r,e,s)=>u(r,typeof e!="symbol"?e+"":e,s);(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const i of document.querySelectorAll('link[rel="modulepreload"]'))c(i);new MutationObserver(i=>{for(const t of i)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&c(n)}).observe(document,{childList:!0,subtree:!0});function s(i){const t={};return i.integrity&&(t.integrity=i.integrity),i.referrerPolicy&&(t.referrerPolicy=i.referrerPolicy),i.crossOrigin==="use-credentials"?t.credentials="include":i.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function c(i){if(i.ep)return;i.ep=!0;const t=s(i);fetch(i.href,t)}})();class m{constructor(e,s){a(this,"_cardsContainer");a(this,"_menu");this._cardsContainer=e,this._menu=s,this._init()}_init(){this._menu.querySelectorAll(".menu-item").forEach(s=>{s.onclick=()=>{var i;const c=+((i=s.dataset)==null?void 0:i.count);if(!isNaN(+c))this._cardsContainer.innerHTML=this._createCards(c);else{const t=this._menu.querySelector(".side-menu__container");this._createMenuItems().forEach(d=>t.append(d))}}})}_createCards(e){if(e===0)return"";let s=`
+var _=Object.defineProperty;var u=(r,e,s)=>e in r?_(r,e,{enumerable:!0,configurable:!0,writable:!0,value:s}):r[e]=s;var a=(r,e,s)=>u(r,typeof e!="symbol"?e+"":e,s);(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const i of document.querySelectorAll('link[rel="modulepreload"]'))c(i);new MutationObserver(i=>{for(const t of i)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&c(n)}).observe(document,{childList:!0,subtree:!0});function s(i){const t={};return i.integrity&&(t.integrity=i.integrity),i.referrerPolicy&&(t.referrerPolicy=i.referrerPolicy),i.crossOrigin==="use-credentials"?t.credentials="include":i.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function c(i){if(i.ep)return;i.ep=!0;const t=s(i);fetch(i.href,t)}})();class m{constructor(e,s){a(this,"_cardsContainer");a(this,"_menu");this._cardsContainer=e,this._menu=s,this._init()}_init(){this._menu.querySelectorAll(".menu-item").forEach(s=>{s.onclick=()=>{var i;const c=+((i=s.dataset)==null?void 0:i.count);if(!isNaN(+c))this._cardsContainer.innerHTML=this._createCards(c);else{const t=this._menu.querySelector(".side-menu__container");this._createMenuItems().forEach(l=>t.append(l))}}})}_createCards(e){if(e===0)return"";let s=`
             <div class="card" tabindex="0">
                 <div class="card__body">
                     <div class="card__image">
                         <picture class="card__picture">
-                            <source media="(max-width: 639px)" srcset="public/img_5_600x600.png">
-                            <source media="(max-width: 1023px)" srcset="public/img_4_800x800.jpg">
-                            <img class="card__picture__img" src="public/img_1.webp" />
+                            <source media="(max-width: 639px)" srcset="img_5_600x600.png">
+                            <source media="(max-width: 1023px)" srcset="img_4_800x800.jpg">
+                            <img class="card__picture__img" src="img_1.webp" />
                         </picture>
                         <div class="card__image__scroll scroll">
                             <span class="scroll__arrow"></span>
                             <span class="scroll__thumb"></span>
                             <svg class="scroll__icon-container" xmlns="http://www.w3.org/2000/svg" width="40" height="40" viewBox="0 0 40 40">
-                                <use class="scroll__icon" href="public/icons.svg#hand-index-icon"></use>
+                                <use class="scroll__icon" href="icons.svg#hand-index-icon"></use>
                             </svg>
                         </div>
                     </div>
@@ -57,4 +57,4 @@ var _=Object.defineProperty;var u=(r,e,s)=>e in r?_(r,e,{enumerable:!0,configura
                 <div class="side-menu__item__content">
                     <span>What is Lorem Ipsum?</span>
                 </div>
-            `,e.push(c)}return e}}const o=document.querySelector(".cards"),l=document.querySelector(".side-menu");o&&l&&new m(o,l);
+            `,e.push(c)}return e}}const o=document.querySelector(".cards"),d=document.querySelector(".side-menu");o&&d&&new m(o,d);
